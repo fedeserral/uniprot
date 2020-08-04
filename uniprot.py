df57767d1e2be0ea20962377951fd736e94950d8
fedeserral <62260723+fedeserral@users.noreply.github.com>
2020-08-04 14:24:07 -0300

    Update uniprot.py

--- uniprot.py
+++ uniprot.py
@@ -15,7 +15,7 @@ url = 'https://www.uniprot.org/'
 def _retrieve(query, format='fasta'):
     """_retrieve is not meant for use with the python interface, use `retrieve`
     instead"""
-   
+    query = list(set(query.split()))
   
     return map(query, "ACC+ID", "ACC", format=format) 
 
